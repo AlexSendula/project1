e81b266b0704f02826e70ae5a623a2bc51598572
DylanvanderStam <dylanvanderstam@hotmail.com>
2021-03-16 16:41:02 +0100

    Methode om te kijken welke examens een student gehaald heeft.

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,12 +1,12 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Student {
     private String voorNaam;
     private String achterNaam;
     private Integer studentNr;
-    private ArrayList<String> behaaldeExamens;
-
+    private static ArrayList<String> behaaldeExamens = new ArrayList<String>();
     private static ArrayList<Student> studentLijst = new ArrayList<Student>(); //toevoeging studenten arraylist
 
     public Student(String voorNaam, String achterNaam, Integer studentNr) {
@@ -28,17 +28,28 @@ public class Student {
         return studentNr;
     }
 
-    public ArrayList<String> getBehaaldeExamens() {
+    public static ArrayList<String> getBehaaldeExamens(Integer studentNr) {
         return behaaldeExamens;
     }
 
+    public static void toStringBehaaldeExamens(){
+        System.out.println("Studentnummer: ");
+        Scanner scn = new Scanner(System.in);
+        Integer studentNr = scn.nextInt();
+        getBehaaldeExamens(studentNr);
+
+        for(String temp : behaaldeExamens){
+            System.out.println(temp);
+        }
+        Main.menu(1);
+    }
 
     public static void studentAanmaken(String voorNaam, String achterNaam, Integer studentNr) {
         new Student(voorNaam, achterNaam, studentNr);
+        Main.menu(1);
     }
 
     public static void studentVerwijderen(Integer studentNr) {
         studentLijst.removeIf(e -> (e.getStudentNr().equals(studentNr)));
-
     }
 }
