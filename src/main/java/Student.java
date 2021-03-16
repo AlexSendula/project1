ca653ccf239acb716961072741dfd663fe77fb99
meltvirus <74384292+meltvirus@users.noreply.github.com>
2021-03-16 15:47:47 +0100

    Arraylist studentLijst, studentAanmaken en studentVerwijderen methodes toegevoegd

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Student {
@@ -6,21 +7,38 @@ public class Student {
     private Integer studentNr;
     private ArrayList<String> behaaldeExamens;
 
+    private static ArrayList<Student> studentLijst = new ArrayList<Student>(); //toevoeging studenten arraylist
+
     public Student(String voorNaam, String achterNaam, Integer studentNr) {
         this.voorNaam = voorNaam;
         this.achterNaam = achterNaam;
         this.studentNr = studentNr;
+        studentLijst.add(this);
     }
+
     public String getVoorNaam() {
         return voorNaam;
     }
+
     public String getAchterNaam() {
         return achterNaam;
     }
+
     public Integer getStudentNr() {
         return studentNr;
     }
+
     public ArrayList<String> getBehaaldeExamens() {
         return behaaldeExamens;
+    }
+
+
+    public static void studentAanmaken(String voorNaam, String achterNaam, Integer studentNr) {
+        new Student(voorNaam, achterNaam, studentNr);
+    }
+
+    public static void studentVerwijderen(Integer studentNr) {
+        studentLijst.removeIf(e -> (e.getStudentNr().equals(studentNr)));
+
     }
 }
