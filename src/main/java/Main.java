7978a60b3bbb6ff0aa3114d0faf9faf4d7459094
Alex <a.sendula@gmail.com>
2021-03-16 13:08:12 +0100

    Toegevoegd: menu en exit methode Moet nog een invalid paramater exception toevoegen.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,88 @@
+import java.security.InvalidParameterException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        menu(0);
     }
+
+    public static void menu(int leegScherm) throws InvalidParameterException {
+        /**
+         This is the menu interface of the program.
+         1 = will flush screen before showing menu interface.
+         0 = will directly show menu interface.
+         @param Allows for 1 or 0 as input.
+         **/
+
+        //Missing InvalidParameterException, please finish this.
+        if (leegScherm == 1 || leegScherm == 0) {
+            throw new InvalidParameterException();
+        }
+
+        if (leegScherm == 1) {
+            leegScherm();
+        }
+
+        System.out.println("\n");
+        System.out.println("1) Lijst met examens");
+        System.out.println("2) Lijst met studenten");
+        System.out.println("3) Nieuwe student inschrijven");
+        System.out.println("4) Student verwijderen");
+        System.out.println("5) Examen afnemen");
+        System.out.println("6) Welke examens heeft student gehaald?");
+        System.out.println("7) Welke student heeft de meeste examens gehaald?");
+        System.out.println("0) exit");
+
+        int menuKeuze = 0;
+
+        try {
+            Scanner scanner = new Scanner(System.in);
+            menuKeuze = scanner.nextInt();
+        } catch (Exception ignored) {
+            menu(1);
+        }
+
+        //Voeg hier de methods toe aan de hand van outputlines hierboven.
+        if (menuKeuze >= 0 && menuKeuze <=7) {
+            switch (menuKeuze) {
+                case 0:
+                    menu(1);
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+            }
+        } else {
+            menu(1);
+        }
+    }
+
+    public static void leegScherm() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("\n");
+        }
+    }
+
+    public static void sluitProgramma() {
+        System.exit(0);
+    }
+
 }
