28a0f509a5843f1f2271978d679b9c1073e4d842
Alex Rojas Lopez <g.alexrl03@gmail.com>
2021-03-16 13:54:01 +0100

    Examen class aangemaakt.

---
+++ src/main/java/Examen.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Examen {
+    private Student gemaaktDoor;
+    private Vraag bestaatUit;
+    private String naam;
+    private Double quotering;
+    private Integer examenLengte;
+    private ArrayList<Vraag> vragenLijst;
+
+    public Examen(String naam, Double quotering, Integer examenLengte, Vraag[] vraag){
+        this.naam = naam;
+        this.quotering = quotering;
+        this.examenLengte = examenLengte;
+
+    }
+    public String getNaam() {
+        return naam;
+    }
+    public Double getQuotering() {
+        return quotering;
+    }
+    public Integer getExamenLengte() {
+        return examenLengte;
+    }
+    public ArrayList<Vraag> getVragenLijst() {
+        return vragenLijst;
+    }
+}
