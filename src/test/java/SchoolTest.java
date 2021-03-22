06d0efbadaa20ac065a0790a79b6c121659659dc
DylanvanderStam <dylanvanderstam@hotmail.com>
2021-03-22 19:02:09 +0100

    Test classes aangemaakt.

---
+++ src/test/java/SchoolTest.java
@@ -0,0 +1,32 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SchoolTest {
+
+    @Test
+    void studentAanmaken() {
+    }
+
+    @Test
+    void studentVerwijderen() {
+    }
+
+    @Test
+    void getStudenten() {
+    }
+
+    @Test
+    void getTopStudent() {
+    }
+
+    @Test
+    void getStudentByNr() {
+    }
+
+    @Test
+    void getBehaaldeExamens() {
+    }
+}
