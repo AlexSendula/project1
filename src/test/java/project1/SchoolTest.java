bc775a3413845bc4f49d240723392dbfd35d3e2f
DylanvanderStam <dylanvanderstam@hotmail.com>
2021-03-23 12:17:48 +0100

    Testcases geupdate.

--- src/test/java/project1/SchoolTest.java
+++ src/test/java/project1/SchoolTest.java
@@ -4,19 +4,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SchoolTest {
     @Test
     void studentAanmakenTest() {
-        InputStream sysInBackup = System.in;
-        ByteArrayInputStream in = new ByteArrayInputStream("Test, Test, 12345678".getBytes());
+        InputStream sysInBackup = null;
+        ByteArrayInputStream in = new ByteArrayInputStream("Voornaam, Achternaam, 12345678".getBytes());
         System.setIn(in);
         School.studentAanmaken();
 
-        assertTrue(!School.studentLijst.isEmpty());
+        assertEquals(School.studentLijst.get(0).getVoorNaam(), "Voornaam");
+        assertEquals(School.studentLijst.get(0).getAchterNaam(), "Achternaam");
+        assertEquals(School.studentLijst.get(0).getStudentNr(), 12345678);
+
+        System.setIn(sysInBackup);
     }
 
     @Test
@@ -38,12 +44,26 @@ class SchoolTest {
 
     @Test
     void getTopStudentTest() {
+        Student test = new Student("Voornaam", "Achternaam", 12345678);
+        test.setBehaaldeExamens("Rekenen");
+        Student test2 = new Student("Voornaam2", "Achternaam2", 87654321);
+        test2.setBehaaldeExamens("Rekenen");
+        test2.setBehaaldeExamens("Engels");
+
+        ByteArrayOutputStream testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+        School.getTopStudent();
+
+        assertTrue(testOut.toString().contains("Voornaam2"));
+        assertFalse(testOut.toString().contains("12345678"));
+        assertFalse(testOut.toString().contains("Niemand"));
     }
 
     @Test
     void getStudentByNrTest() {
-        Student test = new Student("Dylan", "van der Stam", 18094171);
-        Assertions.assertTrue(School.getStudentByNr(18094171).equals(test));
+        Student test = new Student("Voornaam", "Achternaam", 12345678);
+        assertEquals(School.getStudentByNr(12345678), test);
+        assertThrows(NullPointerException.class, () -> School.getStudentByNr(87654321));
     }
 
     @Test
