88622dab562528af28ea5483baf0e1bb4667516e
Alex <a.sendula@gmail.com>
2021-03-23 10:48:02 +0100

    Test cases bug fix

---
+++ src/test/java/project1/ExamenTest.java
@@ -0,0 +1,23 @@
+package project1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExamenTest {
+    @Test
+    void getNaamTest() {
+    }
+
+    @Test
+    void getQuoteringTest() {
+    }
+
+    @Test
+    void getExamenLengteTest() {
+    }
+
+    @Test
+    void getVragenLijstTest() {
+    }
+}
---
+++ src/test/java/project1/VraagTest.java
@@ -0,0 +1,17 @@
+package project1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VraagTest {
+    @Test
+    void getVraagTest() {
+
+    }
+
+    @Test
+    void getAntwoordTest() {
+
+    }
+}
