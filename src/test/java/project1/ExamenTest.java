5d0cb5f8488eb69dbe7347f48e9db2f8ce2e87dd
AbdullahAlhayek <abd1996hk@gmail.com>
2021-03-25 13:21:59 +0100

    JUnitTest Class ExamenTest

--- src/test/java/project1/ExamenTest.java
+++ src/test/java/project1/ExamenTest.java
@@ -2,22 +2,39 @@ package project1;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExamenTest {
+
+
     @Test
-    void getNaamTest() {
+    public void getNaam() {
+        Examen examen = new Examen("OPT1" , 10.0,20,Examen.vragenLijst);
+        assertEquals("OPT1",examen.getNaam());
+        assertNotEquals("opt1",examen.getNaam());
     }
 
     @Test
-    void getQuoteringTest() {
+    public void getQuotering(){
+        Examen examen = new Examen("Wiskunde",10.0,100, Examen.vragenLijst);
+        assertEquals(10.0,examen.getQuotering());
+        assertNotEquals(20 ,examen.getExamenLengte());
     }
 
     @Test
-    void getExamenLengteTest() {
+    public void getExamenLengte() {
+        Examen examen = new Examen("OPT1",10.0,100, Examen.vragenLijst);
+        assertEquals(100,examen.getExamenLengte());
+        assertNotEquals(101 , examen.getExamenLengte());
     }
 
     @Test
-    void getVragenLijstTest() {
+    public void getVragenLijst() {
+        Examen examen = new Examen("OPT2",10.0,100, Examen.vragenLijst);
+        assertEquals(examen.getVragenLijst(),examen.getVragenLijst());
+
     }
 }
